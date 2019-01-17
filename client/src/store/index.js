af6ce1b5b04c7dc687d97bf3e4f97b705c35c569
Tony Johnson <tony@tosp.net.au>
2019-01-17 19:05:14 +1100

    Change socket connection to production

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,16 +6,13 @@ import socketMiddleware from 'redux-socket.io-middleware'
 import combinedReducers from '../reducers'
 
 const loggerMiddleware = createLogger()
-// const io = socket.connect('http://www.tosp.net.au:3008')
-const io = socket.connect('http://localhost:3008')
+const io = socket.connect('http://www.tosp.net.au:3008')
+// const io = socket.connect('http://localhost:3008')
 
 const enhancer = compose(
-  applyMiddleware(
-    loggerMiddleware,
-    socketMiddleware(io)
-  )
+  applyMiddleware(loggerMiddleware, socketMiddleware(io))
 )
 
-export default (initialState) => {
+export default initialState => {
   return createStore(combinedReducers, initialState, enhancer)
 }
